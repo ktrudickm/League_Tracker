f5b2b4af8b44718b67e0a345ff0bbb1bec0a231b
Nihal Abdalla <miz46769@gmail.com>
2021-06-17 17:52:17 -0400

    update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
 
 app.post("/submit", ({ body }, res) => {
   User.create(body)
