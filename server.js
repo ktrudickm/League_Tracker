de71550f7bc5070736fb8b04cd25934b60cf3685
Kendall <ktrudickm@gmail.com>
2021-06-17 22:02:33 -0400

    Started adding players to seed file

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ mongoose.connect(process.env.MONGOD_URI ||
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./leagueDB.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -15,17 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-
-app.post("/submit", ({ body }, res) => {
-  User.create(body)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
