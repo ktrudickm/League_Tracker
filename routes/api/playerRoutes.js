0cf7f16b856515842d72181616487dc146541271
Keon Pourboghrat <kekons4@gmail.com>
2021-06-19 14:37:18 -0400

    added the rest of player routes

--- routes/api/playerRoutes.js
+++ routes/api/playerRoutes.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
-const { Player } = require('../../models');
+const { Player, Team } = require('../../models');
 
 // Fetches single player data
 router.get('/:id', (req, res) => {
     Player.findById({_id: req.params.id})
         .then(dbPlayer => {
             res.status(200).json(dbPlayer);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json(err);
+        });
+});
+
+// Fetches all players from team
+router.get('/byteam/:teamid', (req, res) => {
+    try {
+        const players = Player.find({team_key: req.params.teamid});
+        if(!players) res.status(404).json({message: "No players on such team"});
+        res.status(200).json(players);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+// Creates new player
+router.post('/add', ({body}, res) => {
+    Player.create(body)
+        .then(data => {
+            res.status(200).json(data);
         })
         .catch(err => {
             console.error(err);
