7724e339d05d6c7c285846f9c8f30dbea4970540
Maxson Green <maxson.green@gmail.com>
2021-06-24 19:45:07 -0400

    Updated navbar styling and created PlayerPage component

--- client/src/components/NavBar/navBar.jsx
+++ client/src/components/NavBar/navBar.jsx
@@ -19,19 +19,22 @@ const NavBar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div
+          className="collapse navbar-collapse justify-content-end px-4"
+          id="navbarNav"
+        >
           <ul className="navbar-nav">
             <li className="nav-item">
               <NavLink className="nav-link" aria-current="page" to="/">
                 PLACEHOLDER
               </NavLink>
             </li>
-            <li className="nav-item">
+            <li className="nav-item px-2">
               <NavLink className="nav-link" to="/s">
                 PLACEHOLDER
               </NavLink>
             </li>
-            <form className="d-flex">
+            <form className="d-flex justify-content-end">
               <input
                 className="form-control me-2"
                 type="search"
@@ -42,7 +45,7 @@ const NavBar = () => {
                 Search
               </button>
             </form>
-            <li className="nav-item">
+            <li className="nav-item px-2">
               <NavLink className="nav-link" to="/login">
                 Login/Sign-Up
               </NavLink>
