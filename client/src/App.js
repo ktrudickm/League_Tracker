b739f49e527cd02e3cfa036cbd62ae3fa89c1231
Kendall <ktrudickm@gmail.com>
2021-06-26 12:52:01 -0400

    Page edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import NavBar from "./components/NavBar/navBar";
 import PlayerPage from "./components/PlayerPage/index";
+import RegisterPlayer from "./components/RegisterPage/RegisterPlayer";
+import LoginPage from "./components/LoginPage/LoginPage";
 import "./App.css";
 
 function App() {
