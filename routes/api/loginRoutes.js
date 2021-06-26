02265d5808c46392ea3876e11e1a347f459a94cd
Keon Pourboghrat <kekons4@gmail.com>
2021-06-26 11:15:32 -0400

    loginRoutes are completed... for now, probably done though

--- routes/api/loginRoutes.js
+++ routes/api/loginRoutes.js
@@ -20,16 +20,36 @@ router.post('/', async(req, res) => {
         }
     } catch (err) {
         console.error(err);
-        res.status(400).json(err);
+        res.status(400).json({message: "An ERROR occurred when logging in"});
     }
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', async ({ body }, res) => {
+    try {
+        const signupData = await Player.create(body);
 
+        req.session.save(() => {
+            req.session.username = signupData.username;
+            req.session.logged_in = true;
+            req.session.isAdmin = loginData.isAdmin;
+            req.session.image = loginData.image;
+
+            res.status(200).json(signupData);
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "An ERROR occured when signing up"});
+    }
 });
 
-router.post('/logout', async (req, res) => {
-    
+router.post('/logout', (req, res) => {
+    if(req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
